ce4448e902985527101e0966889bd75cce7edd95
Nathalie  Dune <nathydune@gmail.com>
2022-12-12 16:54:49 +0100

    exercice

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const app = express();
 
 app.use(express.json());
 
-const port = process.env.APP_PORT ?? 5000;
+const port = process.env.APP_PORT ?? 5001;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
+
 
 app.get("/", welcome);
 
@@ -22,13 +23,19 @@ app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
+
+
 const userHandlers = require("./userHandlers");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", userHandlers.postUser);
+app.post("/api/users", userHandlers.postUser)
 app.put("/api/users/:id", userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
+
+const {hashPassword} = require("./auth.js");
+
+app.post("/api/users", hashPassword, userHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
